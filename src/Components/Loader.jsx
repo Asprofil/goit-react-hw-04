88c2a5aa709dce41086defb6c5386895cd04c911
Asprofil <adober.demchenko@gmail.com>
2024-08-09 11:50:51 +0300

    Delete src directory

--- src/Components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Oval } from 'react-loader-spinner';
-
-const Loader = () => {
-  return <Oval color="#00BFFF" height={40} width={40} />;
-};
-
-export default Loader;
