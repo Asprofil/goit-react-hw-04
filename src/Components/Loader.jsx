52137e9949d7de8311aa14b7ed1d39e16a3ce4e0
Asprofil <adober.demchenko@gmail.com>
2024-08-09 11:45:48 +0300

    Initial commit

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Oval } from 'react-loader-spinner';
+
+const Loader = () => {
+  return <Oval color="#00BFFF" height={40} width={40} />;
+};
+
+export default Loader;
